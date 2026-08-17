b3af4d014f961fb4573fdf5579822308867db286
xunyixiadebao <120244668+xunyixiadebao@users.noreply.github.com>
2026-08-17 09:24:25 +0800

    项目创建

---
+++ src/main/java/org/example/djiankang/DjiankangApplication.java
@@ -0,0 +1,13 @@
+package org.example.djiankang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjiankangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DjiankangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/djiankang/DjiankangApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.djiankang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjiankangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
