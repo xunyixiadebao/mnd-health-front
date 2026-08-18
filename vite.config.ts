ba6ac3692d877ec2aa96d5ee848f11e3ae44b285
xunyixiadebao <120244668+xunyixiadebao@users.noreply.github.com>
2026-08-18 19:55:44 +0800

    scssimport变为use

--- vite.config.ts
+++ vite.config.ts
@@ -31,4 +31,23 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    // 配置前端项目所在服务器的IP地址
+    host: "localhost",
+    // 配置前端项目所在服务器的端口号
+    port: 5173,
+
+    // 配置代理
+    // 所有以 /api 开头的请求路径都走vite开发服务器的代理机制。
+    // 你实际的请求路径是：http://localhost:5173/api/test
+    // vite开发服务器代理的结果是：https://localhost:8080/api/test
+    // 这样后端就不会认为是跨域访问了。
+    proxy: {
+      "/api": {
+        target: "https://localhost:8080",
+        changeOrigin: true,
+        secure: false, // vite开发服务器不验证后端服务器的证书（我们HTTPS协议使用的是自签名证书，因此这里不能让Vite开发服务器验证证书，如果验证会失败）
+      },
+    },
+  },
 });
